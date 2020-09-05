225cd9ab234f518e551e40a156de7e128233766a
Gabriela Gonzalez <gabytasmile7@gmail.com>
2020-09-04 21:53:28 -0400

    se probo un card en post

--- pages/post.vue
+++ pages/post.vue
@@ -1,5 +1,42 @@
 <template>
-  <h1>Soy un post</h1>
+  <v-card
+    class="mx-auto"
+    max-width="400"
+  >
+    <v-img
+      class="white--text align-end"
+      height="200px"
+      src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+    >
+      <v-card-title>Titulo de prueba</v-card-title>
+    </v-img>
+
+    <v-card-subtitle class="pb-0">
+      hello
+    </v-card-subtitle>
+
+    <v-card-text class="text--primary">
+      <div>Whitehaven Beach</div>
+
+      <div>caracteristicas de la imagen</div>
+    </v-card-text>
+
+    <v-card-actions>
+      <v-btn
+        color="orange"
+        text
+      >
+        Share
+      </v-btn>
+
+      <v-btn
+        color="orange"
+        text
+      >
+        Explore
+      </v-btn>
+    </v-card-actions>
+  </v-card>
 </template>
 
 <script>
